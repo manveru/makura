ea03df72d204c0fa24ced69c6ebe0a7d2e0c34d0
Michael Fellinger <m.fellinger@gmail.com>
2010-09-30 10:02:39 +0900

    Make escape compatible with unicode

--- lib/makura.rb
+++ lib/makura.rb
@@ -35,9 +35,19 @@ module Makura
 
   # From Rack
   def escape(s)
-    s.to_s.gsub(/([^ a-zA-Z0-9_.-]+)/n) {
-      '%'+$1.unpack('H2'*$1.size).join('%').upcase
+    s.to_s.gsub(/([^ a-zA-Z0-9_.-]+)/u) {
+      '%'+$1.unpack('H2'*bytesize($1)).join('%').upcase
     }.tr(' ', '+')
+  end
+
+  if "".respond_to?(:bytesize)
+    def bytesize(string)
+      string.bytesize
+    end
+  else
+    def bytesize(string)
+      string.size
+    end
   end
 
   def pretty_from_md5(md5)
